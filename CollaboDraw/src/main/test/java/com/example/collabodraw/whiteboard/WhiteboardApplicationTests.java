91d267c46acf173d6f956e18c6fbc58b17f34413
Pragnya <prag1704@gmail.com>
2025-09-04 09:39:32 +0530

    Add files via upload

---
+++ CollaboDraw/src/main/test/java/com/example/collabodraw/whiteboard/WhiteboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.colabodraw.whiteboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
