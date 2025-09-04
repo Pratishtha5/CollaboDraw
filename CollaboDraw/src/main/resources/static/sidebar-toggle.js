607cc71863b31050b0ca66160b6fb4b30f0b67cf
Pragnya <prag1704@gmail.com>
2025-09-04 09:40:06 +0530

    Delete CollaboDraw directory

--- CollaboDraw/src/main/resources/static/sidebar-toggle.js
+++
@@ -1,7 +0,0 @@
-const sidebar = document.getElementById('sidebar');
-const logo = sidebar.querySelector('.logo');
-
-logo.style.cursor = 'pointer';
-logo.addEventListener('click', () => {
-  sidebar.classList.toggle('collapsed');
-});
