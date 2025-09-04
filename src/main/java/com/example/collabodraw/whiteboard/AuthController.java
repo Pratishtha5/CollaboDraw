98a631c9317ef3be240aa46962eafe6737f61c62
Pragnya Khandelwal <prag1704@gmail.com>
2025-09-04 10:00:13 +0530

    Initial import

---
+++ src/main/java/com/example/collabodraw/whiteboard/AuthController.java
@@ -0,0 +1,13 @@
+package com.example.collabodraw.whiteboard;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+    @GetMapping("/auth")
+    public String login() {
+        return "auth"; 
+    }
+}
+
